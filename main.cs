d48abc25213e36de62a1b56e4fe5b48e025ad793
Dan Parker <dan.parker@revolv3.com>
2024-12-17 11:49:44 -0800

    Putting some really bad code in

--- main.cs
+++ main.cs
@@ -9,6 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Hello, World!");
 
             AskAndPrintName();
+            FailCodeCheck(1, "'5", 3);
         }
 
         static void AskAndPrintName()
@@ -16,6 +17,28 @@ namespace HelloWorld
             Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello, " + name + "!");
+        }
+
+        static int Task FailCodeCheck(int customerId, string badCodeCanBeHere, int? maybeInt)
+        {
+
+            var requestRoutingRule = $"SELECT * FROM routing.ProcessorRoutingRule where MerchantId={maybeInt}";
+            var merchantProcessor = $"SELECT* FROM r3.MerchantProcessor where MerchantId = {maybeInt}" + badCodeCanBeHere;
+
+            int badInt = Convert.ToInt32(badCodeCanBeHere);
+
+            int newInt = Convert.ToInt32(maybeInt) + badInt;
+
+            double dontDothis = 5/Convert.ToInt32(maybeInt);
+            dontDothis += badInt + newInt;
+            Console.WriteLine("Fail=" + newInt);
+            Console.WriteLine("Fail=" + merchantProcessor);
+
+            if (maybeInt > 3){
+                return 6;
+            }
+            else 
+                return maybeInt;
         }
     }
 }
